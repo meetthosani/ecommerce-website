157c17b2e0e0870e9518d2d858cdb28875093f56
meetthosani <meet.thosani@gmail.com>
2024-04-14 16:55:30 -0400

    added popular component

---
+++ src/components/popular/Popular.js
@@ -0,0 +1,26 @@
+import "./Popular.css";
+import data_product from "../assets/data";
+import Item from "../item/Item";
+function Popular() {
+  return (
+    <div className="popular">
+      <h1>Popular in Women</h1>
+      <hr />
+      <div className="popular-item">
+        {data_product.map((index, item) => {
+          return (
+            <Item
+              key={index}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              old_price={item.old_price}
+              new_price={item.new_price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+export default Popular;
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,8 +1,10 @@
 import Hero from "../components/hero/Hero";
+import Popular from "../components/popular/Popular";
 function Shop() {
   return (
     <div>
       <Hero />
+      <Popular />
     </div>
   );
 }
